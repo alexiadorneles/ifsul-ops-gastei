3fa2318d3dff17c36b39fadadfbbae5d42a09bb3
Marcelo Henrique <marcelohs180@gmail.com>
2017-06-20 23:58:07 -0300

    refactor(main-module): realizar importação de modulos ao app.

--- app.module.js
+++ app.module.js
@@ -1,1 +1,12 @@
-angular.module('app', ['ngRoute']);
+import angular from 'angular';
+import ngRoute from 'angular-route';
+import './css/style.css';
+
+import routing from './app.route';
+
+import cadastro from './cadastro';
+import login from './login'
+
+angular.module('app', [ngRoute, cadastro, login])
+    .config(routing)
+

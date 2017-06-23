46f499bb61146f9433ceaa7c31b118da50a9f520
Marcelo Henrique <marcelohs180@gmail.com>
2017-06-23 04:23:35 -0300

    feat(backend): adicionar estrutura basica do server.

---
+++ backend/src/main/java/com/ifdevs/opsgastei/OpsgasteiApplication.java
@@ -0,0 +1,12 @@
+package com.ifdevs.opsgastei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpsgasteiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpsgasteiApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/ifdevs/opsgastei/OpsgasteiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ifdevs.opsgastei;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpsgasteiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

4000dcc6961528e6d92b39bdb923e7052eeb86b0
Marcelo Henrique <marcelohs180@gmail.com>
2017-06-23 05:17:59 -0300

    feat(usuario): criar model.

---
+++ backend/src/test/java/com/ifdevs/opsgastei/model/Usuario.java
@@ -0,0 +1,63 @@
+package com.ifdevs.opsgastei.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by mhenrique on 6/23/17.
+ */
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotNull
+    @Column(nullable = false)
+    private String senha;
+
+    @NotNull
+    @Column(nullable = false)
+    @Min(0)
+    private Double salario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+}

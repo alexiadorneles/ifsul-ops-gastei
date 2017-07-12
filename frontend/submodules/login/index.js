b9b74a64179cd5f7cddc73a5aef01b1ca65fb312
Marcelo <marcelohs180@gmail.com>
2017-07-12 11:23:53 -0300

    feat(login): adicionar dependencias e servicos ao modulo.

--- frontend/submodules/login/index.js
+++ frontend/submodules/login/index.js
@@ -1,10 +1,20 @@
 import angular from 'angular'
 import ngRoute from 'angular-route'
+import angularAuth0 from 'angular-auth0'
 
+import './login.scss'
 import router from './login.routes'
 import LoginController from './login.controller'
+import loginService from './login.service'
 
-export default angular.module('app.login', [ngRoute])
+export default angular.module('app.login', [ngRoute, angularAuth0])
     .config(router)
     .controller("LoginController", LoginController)
-    .name
+    .controller('CallbackController', () => {})
+    .service("loginService", loginService)
+    .run(run)
+    .name;
+
+function run(loginService) {
+    loginService.handleAuthentication();
+}

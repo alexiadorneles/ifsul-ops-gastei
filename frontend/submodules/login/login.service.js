2a03976b4394ca93daf314e37061c9c0954132b3
Marcelo <marcelohs180@gmail.com>
2017-07-12 11:20:53 -0300

    feat(login-service): criar servico de Login.

---
+++ frontend/submodules/login/login.service.js
@@ -0,0 +1,49 @@
+export default function loginService($location, angularAuth0, $timeout) {
+
+    function login() {
+      angularAuth0.authorize();
+    }
+
+    function handleAuthentication() {
+      angularAuth0.parseHash(function(err, authResult) {
+        if (authResult && authResult.accessToken && authResult.idToken) {
+          setSession(authResult);
+          console.log(authResult);
+          $location.path('/home');
+        } else if (err) {
+          $timeout(function() {
+            $location.path('/home');
+          });
+        }
+      });
+    }
+
+    function setSession(authResult) {
+      // Set the time that the access token will expire at
+      let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+      localStorage.setItem('access_token', authResult.accessToken);
+      localStorage.setItem('id_token', authResult.idToken);
+      localStorage.setItem('expires_at', expiresAt);
+    }
+
+    function logout() {
+      // Remove tokens and expiry time from localStorage
+      localStorage.removeItem('access_token');
+      localStorage.removeItem('id_token');
+      localStorage.removeItem('expires_at');
+    }
+
+    function isAuthenticated() {
+      // Check whether the current time is past the
+      // access token's expiry time
+      let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+      return new Date().getTime() < expiresAt;
+    }
+
+    return {
+      login: login,
+      handleAuthentication: handleAuthentication,
+      logout: logout,
+      isAuthenticated: isAuthenticated
+    }
+}

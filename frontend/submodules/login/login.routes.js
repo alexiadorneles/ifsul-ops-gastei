d6c6488d43e3d69cddd5d4c79650423c5e8e6288
Marcelo <marcelohs180@gmail.com>
2017-07-12 11:19:28 -0300

    config(auth0): rotas e inicialização.
    - configs do Auth0
    - rotas de callback

--- frontend/submodules/login/login.routes.js
+++ frontend/submodules/login/login.routes.js
@@ -1,7 +1,23 @@
-export default function routing ($routeProvider) {
-  $routeProvider
-  .when('/login', {
-    controller: 'LoginController',
-    templateUrl: '/submodules/login/login.html'
-  })
+export default function routing ($routeProvider, angularAuth0Provider) {
+    $routeProvider
+    .when('/login', {
+      controller: 'LoginController',
+      controllerAs: 'vm',
+      templateUrl: '/submodules/login/login.html'
+    })
+    $routeProvider
+    .when('/callback', {
+      controller: 'CallbackController',
+      templateUrl: '/submodules/login/callback.html'
+    })
+
+    // Initialization for the angular-auth0 library
+    angularAuth0Provider.init({
+      clientID: '4fzJ7Zcm7WdmDCw15KsyRfQTkAYWW6Oc',
+      domain: 'opsgastei.auth0.com',
+      responseType: 'token id_token',
+      audience: 'https://opsgastei.auth0.com/userinfo',
+      redirectUri: 'http://localhost:9000/callback',
+      scope: 'openid'
+    });
 };

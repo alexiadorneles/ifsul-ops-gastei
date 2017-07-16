088d50940b3090a8c4aece7848e90ebb1f4b497c
Alessandra Henkes <henkesalessandra0@gmail.com>
2017-07-16 18:02:37 -0300

    feat(module): injetar dependencia de arquivado.

--- frontend/app.module.js
+++ frontend/app.module.js
@@ -8,9 +8,10 @@ import routing from './app.route';
 import anterior from './submodules/anterior';
 import objetivo from './submodules/objetivo';
 import add_objetivo from './submodules/add_objetivo';
+import arquivado from './submodules/arquivado';
 import cadastro from './submodules/cadastro';
 import login from './submodules/login'
 
 
-angular.module('app', [ngRoute, anterior, objetivo, add_objetivo, cadastro, login])
+angular.module('app', [ngRoute, anterior, objetivo, add_objetivo, arquivado, cadastro, login])
     .config(routing)

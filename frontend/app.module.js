2983dc4dd117b0c382581c49b1e044e918c17c0a
Alessandra Henkes <henkesalessandra0@gmail.com>
2017-07-18 19:34:22 -0300

    feat(module): injetar dependencia de simulacao no modulo principal.

--- frontend/app.module.js
+++ frontend/app.module.js
@@ -10,9 +10,10 @@ import proximo from './submodules/proximo';
 import objetivo from './submodules/objetivo';
 import add_objetivo from './submodules/add_objetivo';
 import arquivado from './submodules/arquivado';
+import simulacao from './submodules/simulacao';
 import cadastro from './submodules/cadastro';
 import login from './submodules/login'
 
 
-angular.module('app', [ngRoute, anterior, proximo, objetivo, add_objetivo, arquivado, cadastro, login])
+angular.module('app', [ngRoute, anterior, proximo, objetivo, add_objetivo, arquivado, simulacao, cadastro, login])
     .config(routing)

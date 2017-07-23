46b813739c1a13ac975d5248c1c9b12943bea688
Marcelo Henrique <marcelohs180@gmail.com>
2017-07-23 15:04:11 -0300

    refactor(code): identar código.

--- frontend/app.module.js
+++ frontend/app.module.js
@@ -1,19 +1,24 @@
-import angular from 'angular';
-import ngRoute from 'angular-route';
+// import angular libs
+import angular from 'angular'
+import ngRoute from 'angular-route'
+
+// import global styles
 import 'sweetalert/dist/sweetalert.css'
-import './assets/css/style.css';
+import './assets/css/style.css'
 
-import routing from './app.route';
+// import routing configs
+import routing from './app.route'
 
-import anterior from './submodules/anterior';
-import proximo from './submodules/proximo';
-import objetivo from './submodules/objetivo';
-import add_objetivo from './submodules/add_objetivo';
-import arquivado from './submodules/arquivado';
-import simulacao from './submodules/simulacao';
-import cadastro from './submodules/cadastro';
-import login from './submodules/login'
-import gasto_fixo from './submodules/gasto_fixo'
+// import submodules
+import anterior     from './submodules/anterior'
+import proximo      from './submodules/proximo'
+import objetivo     from './submodules/objetivo'
+import add_objetivo from './submodules/add_objetivo'
+import arquivado    from './submodules/arquivado'
+import simulacao    from './submodules/simulacao'
+import cadastro     from './submodules/cadastro'
+import login        from './submodules/login'
+import gasto_fixo   from './submodules/gasto_fixo'
 
 
 angular.module('app', [ngRoute, anterior, proximo, objetivo, add_objetivo, arquivado, simulacao,

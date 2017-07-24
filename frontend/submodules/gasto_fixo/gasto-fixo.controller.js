69fbc563660d9c8e07d67cbc91473351b5d4c2c9
AlexiaDorneles <lecavsalexia@gmail.com>
2017-07-24 20:24:51 -0300

    feat(opcoes): adicionar opções quando click na engine.

--- frontend/submodules/gasto_fixo/gasto-fixo.controller.js
+++ frontend/submodules/gasto_fixo/gasto-fixo.controller.js
@@ -1,4 +1,10 @@
 import swal from 'sweetalert'
 export default function GastoFixoController ($location, $scope) {
 
+  $scope.mostrarOpcoes = mostrarOpcoes;
+
+  function mostrarOpcoes(gastoFixo) {
+    gastoFixo.opcoes = gastoFixo.opcoes ? false : true;
+  }
+
 }

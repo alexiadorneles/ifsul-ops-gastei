9f5a21ccd0106c008e9c55dafcdd14b50825978f
Unknown <schreiner.flavia@hotmail.com>
2017-07-25 17:00:11 -0300

    feat(module): adicionar a dependencia da tela editar_perfil.

--- frontend/app.module.js
+++ frontend/app.module.js
@@ -20,11 +20,12 @@ import simulacao    from './submodules/simulacao'
 import cadastro     from './submodules/cadastro'
 import login        from './submodules/login'
 import gasto_fixo   from './submodules/gasto_fixo'
+import editar_perfil from './submodules/editar_perfil'
 import home         from './submodules/home'
 
 
 angular.module('app', [ngRoute, anterior, proximo, objetivo, add_objetivo, arquivado, simulacao,
-  cadastro, login, gasto_fixo, home])
+  cadastro, login, gasto_fixo, home, editar_perfil])
     .directive('navbar', () => {
       return {
           restrict: 'E',

c74f247321e3efc835edbcf0901ef6e90437a938
Marcelo Henrique <marcelohs180@gmail.com>
2017-07-27 00:54:15 -0300

    refactor(repository): remover anotações do DataRest.

--- backend/src/main/java/com/ifdevs/opsgastei/repository/GastoFixoMesRepository.java
+++ backend/src/main/java/com/ifdevs/opsgastei/repository/GastoFixoMesRepository.java
@@ -2,11 +2,9 @@ package com.ifdevs.opsgastei.repository;
 
 import com.ifdevs.opsgastei.model.GastoFixoMes;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by mhenrique on 6/30/17.
  */
-@RepositoryRestResource(path = "gastoFixoMes", collectionResourceRel = "gastoFixoMes")
 public interface GastoFixoMesRepository extends CrudRepository<GastoFixoMes, Long> {
 }

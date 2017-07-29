4cb2e441eab03f14964bd770975a5f1ed0a325b1
Marcelo Henrique <marcelohs180@gmail.com>
2017-07-29 10:32:33 -0300

    refactor(usuario): remover salario da entidade.
    Com a criação de uma entidade Salario, para saber o salario atual deve-se fazer uma query selecionando o salario com data mais atual. Salario de meses anteriores sao preservados.

--- backend/src/main/java/com/ifdevs/opsgastei/model/Usuario.java
+++ backend/src/main/java/com/ifdevs/opsgastei/model/Usuario.java
@@ -27,17 +27,11 @@ public class Usuario {
     @Column(nullable = false)
     private String senha;
 
-    @NotNull
-    @Column(nullable = false)
-    @Min(0)
-    private Double salario;
-
-    public Usuario(Long id, String nome, String email, String senha, Double salario) {
+    public Usuario(Long id, String nome, String email, String senha) {
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.salario = salario;
     }
 
     public Usuario() {
@@ -73,14 +67,6 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
-    }
-
-    public Double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(Double salario) {
-        this.salario = salario;
     }
 
     public void criptografarSenha() {

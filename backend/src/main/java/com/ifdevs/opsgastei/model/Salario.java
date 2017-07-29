82c736d65ff8fde51a7fce44c80e088939a8d203
Marcelo Henrique <marcelohs180@gmail.com>
2017-07-29 09:50:09 -0300

    feat(salario): criar entidade.

---
+++ backend/src/main/java/com/ifdevs/opsgastei/model/Salario.java
@@ -0,0 +1,47 @@
+package com.ifdevs.opsgastei.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * Created by mhenrique on 7/29/17.
+ */
+@Entity
+public class Salario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private Double valor;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}

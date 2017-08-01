87d928b9a0d433b5d9b64bf223ff47ebfcd92085
AlexiaDorneles <lecavsalexia@gmail.com>
2017-07-31 22:55:33 -0300

    feat(login): adicionar o login e cadastro de usuarios pela tela de inicial.

--- frontend/submodules/inicial/inicial.controller.js
+++ frontend/submodules/inicial/inicial.controller.js
@@ -1,19 +1,60 @@
-export default function InicialController ($location, $scope){
+export default function InicialController ($location, $scope, usuarioService,
+  salarioService, authFactory, $localStorage) {
+    var usuarioGoogle = init();
 
-  $scope.alternarShowPassword = alternarShowPassword;
-  $scope.alternarShowRepeatPassword = alternarShowRepeatPassword;
-  $scope.showPassword = false;
-  $scope.showRepeatPassword = false;
-
-  function alternarShowPassword() {
-    if (!$scope.showPassword) {
-      $scope.showPassword = true;
-    } else {
-      $scope.showPassword = false;
+    function criarUsuario(usuario) {
+      debugger;
+      usuarioService
+      .criar(usuarioGoogle)
+      .then(() => {
+        logarECriarSalario(usuarioGoogle, usuario.salario);
+      });
     }
-  }
 
-  function alternarShowRepeatPassword() {
-    $scope.showRepeatPassword = $scope.showRepeatPassword ? false : true;
-  }
-}
+
+    function logarECriarSalario(usuario, valor) {
+      authFactory.login(usuario)
+      .then(
+        // executa caso dê tudo certo com o login
+        () => {
+          criarSalario(valor);
+          mensagemLogin(usuario.nome, true);
+          localStorage.setItem('nome', usuario.nome);
+        },
+        // executa caso houer algum erro no login
+        () => {
+          mensagemLogin('', false);
+        });
+      };
+
+      function criarSalario(valor) {
+        let salario = {valor: valor, data: new Date()}
+        salarioService.criar(salario);
+      }
+
+      function mensagemLogin(nomeUsuario, sucesso) {
+        if (sucesso) {
+          return swal({
+            title: "Login realizado com sucesso!",
+            text: 'Bem vindo, '  + nomeUsuario + '.',
+            timer: 2000,
+            showConfirmButton: false
+          });
+        } else {
+          swal({
+            title: "Erro ao logar!",
+            text: "Houve algum erro com o login, por favor tente novamente!",
+            type: "error",
+            confirmButtonText: "OK"
+          });
+        }
+      }
+
+      function init() {
+        var usuarioAux = angular.copy($localStorage.usuarioGoogle);
+        delete $localStorage.usuarioGoogle;
+        $scope.criarUsuario = criarUsuario;
+        return usuarioAux;
+      }
+
+    }

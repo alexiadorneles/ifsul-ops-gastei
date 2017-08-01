03f6d056ba4cb94b487e203240988daa33937571
Alessandra Henkes <henkesalessandra0@gmail.com>
2017-07-31 21:27:49 -0300

    feat(module): adicionar dependencia da tela categorias.

--- frontend/app.module.js
+++ frontend/app.module.js
@@ -22,6 +22,7 @@ import simulacao from './submodules/simulacao'
 import cadastro from './submodules/cadastro'
 import login from './submodules/login'
 import gasto_fixo from './submodules/gasto_fixo'
+import categoria from './submodules/categoria'
 import home from './submodules/home'
 
 // import directives
@@ -29,7 +30,7 @@ import navbar from './directives/navbar'
 import menu from './directives/menu'
 
 angular.module('app', [ngRoute, ngstorage, anterior, proximo, objetivo, add_objetivo, arquivado, simulacao,
-  cadastro, login, gasto_fixo, home])
+  categoria, cadastro, login, gasto_fixo, home])
   .directive('navbar', navbar)
   .directive('menu', menu)
   .config(routing)

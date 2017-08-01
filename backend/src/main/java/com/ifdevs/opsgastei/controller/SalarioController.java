850bbf7b55c51a6647006f5408ea8993580bb167
AlexiaDorneles <lecavsalexia@gmail.com>
2017-07-31 22:52:01 -0300

    fix(backend-salario): arrumar controller de salario para atribuir usuario.

--- backend/src/main/java/com/ifdevs/opsgastei/controller/SalarioController.java
+++ backend/src/main/java/com/ifdevs/opsgastei/controller/SalarioController.java
@@ -1,8 +1,12 @@
 package com.ifdevs.opsgastei.controller;
 
 import com.ifdevs.opsgastei.model.Salario;
+import com.ifdevs.opsgastei.model.Usuario;
 import com.ifdevs.opsgastei.service.SalarioService;
+import com.ifdevs.opsgastei.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -15,13 +19,18 @@ public class SalarioController {
     @Autowired
     private SalarioService service;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @GetMapping
     public Iterable<Salario> findAll() {
         return service.findAll();
     }
 
     @PostMapping
-    public Salario save(@RequestBody Salario salario) {
+    public Salario save(@RequestBody Salario salario, @AuthenticationPrincipal User user) {
+        Usuario usuario = usuarioService.findUsuarioByEmail(user.getUsername());
+        salario.setUsuario(usuario);
         return service.save(salario);
     }
 

c29fbf67536366072dc8c01619c185129574c2f0
Marcelo Henrique <marcelohs180@gmail.com>
2017-08-02 02:05:15 -0300

    feat(categorias): buscar e inserir.

--- frontend/submodules/categoria/categoria.controller.js
+++ frontend/submodules/categoria/categoria.controller.js
@@ -1,10 +1,34 @@
 import swal from 'sweetalert'
 
-export default function CategoriaController ($location, $scope){
-  $scope.adicionarCategoria = adicionarCategoria;
-  $scope.addCategoria = false;
+export default function CategoriaController ($location, $scope, categoriaService){
+
+  init();
 
   function adicionarCategoria(){
     $scope.addCategoria = !$scope.addCategoria;
+
+    // Se vai esconder, ação de inserção
+    if ($scope.addCategoria === false && $scope.categoria) {
+
+        // preto, se nao selecionado
+        $scope.categoria.cor = $scope.categoria.cor || "#000000";
+
+        categoriaService.criar($scope.categoria).then( () => {
+            swal("Sucesso!", "Categoria adicionada.", "success");
+        }).then( () => {
+            // TODO usar error handler
+        })
+    }
+  }
+
+  function init(){
+      $scope.adicionarCategoria = adicionarCategoria;
+      $scope.addCategoria = false;
+
+      categoriaService.buscarTodos().then( response => {
+          $scope.categorias = response.data
+      }).then( () => {
+          // TODO usar error handler
+      })
   }
 }

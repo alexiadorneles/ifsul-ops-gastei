a091fdae15244044055e7773f83ce4d5b05ae422
Alessandra Henkes <henkesalessandra0@gmail.com>
2017-08-02 16:56:26 -0300

    feat(service): adicionar service de objetivos ao index de simulacao.

--- frontend/submodules/simulacao/index.js
+++ frontend/submodules/simulacao/index.js
@@ -3,7 +3,9 @@ import ngRoute from 'angular-route'
 
 import './simulacao.css'
 import SimulacaoController from './simulacao.controller'
+import objetivoService from '../../services/objetivo.service'
 
 export default angular.module('app.simulacao', [ngRoute])
   .controller('SimulacaoController', SimulacaoController)
+  .service('objetivoService', objetivoService)
   .name;

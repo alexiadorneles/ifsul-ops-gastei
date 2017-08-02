a15dcfff55f780fe4534e19a8d102085dd14d1f0
Marcelo Henrique <marcelohs180@gmail.com>
2017-08-02 01:21:35 -0300

    refactor(adicionar-categoria): simplificar statement.

--- frontend/submodules/categoria/categoria.controller.js
+++ frontend/submodules/categoria/categoria.controller.js
@@ -1,13 +1,10 @@
 import swal from 'sweetalert'
+
 export default function CategoriaController ($location, $scope){
   $scope.adicionarCategoria = adicionarCategoria;
   $scope.addCategoria = false;
 
   function adicionarCategoria(){
-    if(!$scope.addCategoria){
-        $scope.addCategoria = true;
-    }else{
-        $scope.addCategoria = false;
-    }
+    $scope.addCategoria = !$scope.addCategoria;
   }
 }

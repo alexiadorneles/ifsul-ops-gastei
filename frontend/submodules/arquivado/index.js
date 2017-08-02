03f92ba30bfb2a766b455f0561c0f40330ce9f7d
Alessandra Henkes <henkesalessandra0@gmail.com>
2017-08-02 19:51:57 -0300

    delete(arquivado): deletar view e controller da tela arquivado.

--- frontend/submodules/arquivado/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular'
-import ngRoute from 'angular-route'
-
-import './arquivado.css'
-import ArquivadoController from './arquivado.controller'
-
-export default angular.module('arquivado', [ngRoute])
-  .controller('ArquivadoController', ArquivadoController)
-  .name;

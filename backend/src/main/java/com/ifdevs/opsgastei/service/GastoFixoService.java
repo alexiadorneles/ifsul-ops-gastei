22e608269789711fc50a869cd47b8fc6b3bb389e
Marcelo Henrique <marcelohs180@gmail.com>
2017-08-02 04:15:36 -0300

    feat(gasto-fixo): criar service de encerramento.

--- backend/src/main/java/com/ifdevs/opsgastei/service/GastoFixoService.java
+++ backend/src/main/java/com/ifdevs/opsgastei/service/GastoFixoService.java
@@ -5,6 +5,9 @@ import com.ifdevs.opsgastei.repository.GastoFixoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  *
  * @author alexia.pereira on 07/28/17
@@ -28,6 +31,21 @@ public class GastoFixoService {
     }
 
     public GastoFixo save(GastoFixo gastoFixo) {
+        return repository.save(gastoFixo);
+    }
+
+    public GastoFixo encerrar(GastoFixo enviado, Date encerramento){
+        GastoFixo gastoFixo = repository.findOne(enviado.getId());
+        if (gastoFixo == null){
+            throw new RuntimeException("GastoFixo não encontrado"); // até exception padrao
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(encerramento);
+        calendar.add(Calendar.MONTH, -1);
+        calendar.set(Calendar.DAY_OF_MONTH, 1); // para melhor consistencia da datas
+
+        gastoFixo.setFimData(calendar.getTime());
         return repository.save(gastoFixo);
     }
 

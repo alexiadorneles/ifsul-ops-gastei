a6a28930cefdf72933ec7af54a3b185057aa6457
Marcelo Henrique <marcelohs180@gmail.com>
2017-08-03 03:03:42 -0300

    refactor(routes): rota de saldo.

--- frontend/app.route.js
+++ frontend/app.route.js
@@ -1,91 +1,82 @@
 routing.$inject = ['$routeProvider', '$locationProvider'];
 
 export default function routing ($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true).hashPrefix('')
-  $routeProvider.otherwise({redirectTo: '/login'});
+    $locationProvider.html5Mode(true).hashPrefix('')
+    $routeProvider.otherwise({redirectTo: '/login'});
 
-  $routeProvider
-    .when('/anterior', {
-        controller: 'AnteriorController',
-        templateUrl: '/submodules/anterior/anterior.html',
-        resolve: {
-          autenticado: function (authFactory) {
-            return authFactory.isAutenticadoPromise();
-          }
-        }
-    })
-    .when('/proximo', {
-        controller: 'ProximoController',
-        templateUrl: '/submodules/proximo/proximo.html',
-        resolve: {
-          autenticado: function (authFactory) {
-            return authFactory.isAutenticadoPromise();
-          }
-        }
-    })
-    .when('/categorias', {
-        controller: 'CategoriaController',
-        templateUrl: '/submodules/categoria/categoria.html',
-        resolve: {
-          autenticado: function (authFactory) {
-            return authFactory.isAutenticadoPromise();
-          }
-        }
-    })
-    .when('/objetivo', {
-        controller: 'ObjetivoController',
-        templateUrl: '/submodules/objetivo/objetivo.html',
-        resolve: {
-          autenticado: function (authFactory) {
-            return authFactory.isAutenticadoPromise();
-          }
-        }
-    })
-    .when('/objetivo/adicionar-objetivo', {
-        controller: 'AdicionarObjetivoController',
-        templateUrl: '/submodules/add_objetivo/add_objetivo.html',
-        resolve: {
-          autenticado: function (authFactory) {
-            return authFactory.isAutenticadoPromise();
-          }
-        }
-    })
-    .when('/objetivo/simulacao', {
-        controller: 'SimulacaoController',
-        templateUrl: '/submodules/simulacao/simulacao.html',
-        resolve: {
-          autenticado: function (authFactory) {
-            return authFactory.isAutenticadoPromise();
-          }
-        }
-    })
-    .when('/cadastro', {
-        controller: 'CadastroController',
-        templateUrl: '/submodules/cadastro/cadastro.html'
-    })
-    .when('/login', {
-      controller: 'LoginController',
-      templateUrl: '/submodules/login/login.html'
-    })
-    .when('/gasto-fixo', {
-        templateUrl: '/submodules/gasto_fixo/gasto-fixo.html',
-        resolve: {
-          autenticado: function (authFactory) {
-            return authFactory.isAutenticadoPromise();
-          }
-        }
-    })
-    .when('/home', {
-        controller: 'HomeController',
-        templateUrl: '/submodules/home/home.html'
-    })
-    .when('/inicial', {
-      controller: 'InicialController',
-      templateUrl: '/submodules/inicial/inicial.html',
-      resolve: {
-        logadoComGoogle: function (authFactory) {
-          return authFactory.isLoggedWithGooglePromise();
-        }
-      }
-    })
+    $routeProvider
+        .when('/saldo', {
+            controller: 'SaldoController',
+            templateUrl: '/submodules/saldo/saldo.html',
+            resolve: {
+                autenticado: function (authFactory) {
+                    return authFactory.isAutenticadoPromise();
+                }
+            }
+        })
+        .when('/categorias', {
+            controller: 'CategoriaController',
+            templateUrl: '/submodules/categoria/categoria.html',
+            resolve: {
+                autenticado: function (authFactory) {
+                    return authFactory.isAutenticadoPromise();
+                }
+            }
+        })
+        .when('/objetivo', {
+            controller: 'ObjetivoController',
+            templateUrl: '/submodules/objetivo/objetivo.html',
+            resolve: {
+                autenticado: function (authFactory) {
+                    return authFactory.isAutenticadoPromise();
+                }
+            }
+        })
+        .when('/objetivo/adicionar-objetivo', {
+            controller: 'AdicionarObjetivoController',
+            templateUrl: '/submodules/add_objetivo/add_objetivo.html',
+            resolve: {
+                autenticado: function (authFactory) {
+                    return authFactory.isAutenticadoPromise();
+                }
+            }
+        })
+        .when('/objetivo/simulacao', {
+            controller: 'SimulacaoController',
+            templateUrl: '/submodules/simulacao/simulacao.html',
+            resolve: {
+                autenticado: function (authFactory) {
+                    return authFactory.isAutenticadoPromise();
+                }
+            }
+        })
+        .when('/cadastro', {
+            controller: 'CadastroController',
+            templateUrl: '/submodules/cadastro/cadastro.html'
+        })
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: '/submodules/login/login.html'
+        })
+        .when('/gasto-fixo', {
+            templateUrl: '/submodules/gasto_fixo/gasto-fixo.html',
+            resolve: {
+                autenticado: function (authFactory) {
+                    return authFactory.isAutenticadoPromise();
+                }
+            }
+        })
+        .when('/home', {
+            controller: 'HomeController',
+            templateUrl: '/submodules/home/home.html'
+        })
+        .when('/inicial', {
+            controller: 'InicialController',
+            templateUrl: '/submodules/inicial/inicial.html',
+            resolve: {
+                logadoComGoogle: function (authFactory) {
+                    return authFactory.isLoggedWithGooglePromise();
+                }
+            }
+        })
 };

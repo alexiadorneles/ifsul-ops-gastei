4c01f196fbec745ec71e046af46853c82800a057
Marcelo Henrique <marcelohs180@gmail.com>
2017-08-03 03:01:40 -0300

    refactor(prox-anter): transformar em ver saldo.

--- frontend/submodules/proximo/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular'
-import ngRoute from 'angular-route'
-
-import './proximo.css'
-import ProximoController from './proximo.controller'
-
-export default angular.module('app.proximo', [ngRoute])
-  .controller('ProximoController', ProximoController)
-  .name;
